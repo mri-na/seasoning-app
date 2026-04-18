788175bb35f83c54dfb30b2536d3aee1aa423ea6
mri-na <marina.89260106@gmail.com>
2026-04-18 18:16:30 +0900

    一覧ページから新規作成ページへの導線を追加

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import { recipes } from "./lib/data"
 import Header from "./components/Header";
 import RecipeListCard from "./components/RecipeListCard";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -8,7 +9,9 @@ export default function Home() {
       <Header
         title="味付けテンプレ"
         rightIcon={
-          <img src="/icons/plus.svg" alt="追加" className="w-7 h-7 mr-[10px] mt-[3px]" />
+          <Link href="/recipes/new">
+            <img src="/icons/plus.svg" alt="追加" className="w-7 h-7 mr-[10px] mt-[3px]" />
+          </Link>
         }
         leftIcon={
           <img src="/icons/search.svg" alt="検索" className="w-[30px] h-[30px] ml-[10px] mt-[5px]" />
